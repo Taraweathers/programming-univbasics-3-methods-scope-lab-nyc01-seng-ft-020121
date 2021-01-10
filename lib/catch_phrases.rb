8061d34a53f2cf59c01627daec0315381fc33969
Tara Weathers <tara.n.weathers@gmail.com>
2021-01-10 16:43:31 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,4 +8,9 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
-toadstool
+
+def link
+phrase = "It's Dangerous To Go Alone! Take This."  
+puts phrase
+end
+link
